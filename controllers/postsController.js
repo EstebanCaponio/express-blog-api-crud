c84c0b347701278622d1f2a47cacc6a57c7183d8
EstebanCaponio <estebancaponio@gmail.com>
2025-03-28 19:57:49 +0100

    feat: milestone 2

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,30 +1,57 @@
-const posts = require('../posts');
+const posts = require('../data/posts');
 
 // function index(req, res) {
 //     res.send('Lista delle pizze');
 // }
-function index (req, res){
+function index(req, res) {
     res.json(posts);
 };
 
 function show(req, res) {
-    
-    const id =parseInt(req.params.id);
 
-    const ricetta = posts.find(ricetta=> ricetta.id === id);
+    const id = parseInt(req.params.id);
+
+    const ricetta = posts.find(ricetta => ricetta.id === id);
+
+    if (!ricetta) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            console: 'not found',
+            message: 'ricetta non trovata'
+        })
+    }
     res.json(ricetta);
 }
 function store(req, res) {
-    res.send('Creazione nuova pizza');
+    res.send('Creazione nuova ricetta');
 }
 function update(req, res) {
-    res.send('Modifica integrale della pizza ' + req.params.id);
+    res.send('Modifica integrale della ricetta ' + req.params.id);
 }
 function modify(req, res) {
-    res.send('Modifica parziale della pizza ' + req.params.id);
+    res.send('Modifica parziale della ricetta ' + req.params.id);
 }
 function destroy(req, res) {
-    res.send('Eliminazione della pizza ' + req.params.id);
+    
+    const id = parseInt(req.params.id);
+
+    const ricetta = posts.find(ricetta => ricetta.id === id);
+
+    if (!ricetta) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            console: 'not found',
+            message: 'ricetta non trovata'
+        })
+    }
+    posts.splice(posts.indexOf(ricetta), 1);
+
+    res.sendStatus(204);
+    console.log(posts);
 }
 // esportiamo tutto
 module.exports = { index, show, store, update, modify, destroy }
