13a143582f9728cd3b196a156663066d79616553
EstebanCaponio <estebancaponio@gmail.com>
2025-03-28 13:33:57 +0100

    feat: created app.get

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 2000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('sei nella home');
+})
+
+app.listen(port, () => {
+    console.log(`hei, Example app listening on port ${port}`)
+})
